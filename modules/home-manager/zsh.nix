b9f9a2084ec82e14b9220e6142c1f652c061a1da
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-11-19 10:50:58 +0500

    some cleanup

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -78,5 +78,51 @@
 
   home.packages = with pkgs; [
     zsh-powerlevel10k
+
+    # Git & code tools
+    git
+    delta
+    git-ignore
+    git-secrets
+    gitleaks
+    pass-git-helper
+    license-generator
+
+    # Productivity
+    just
+    process-compose
+    topgrade
+    noti
+
+    # Networking & HTTP
+    curl
+    wget
+    xh
+    aria2
+
+    # File ops & search
+    eza
+    fd
+    ripgrep
+    bat
+    sd
+    duf
+    ouch
+    yazi
+    procs
+
+    # Monitoring
+    btop
+    progress
+    cava
+
+    # Fun / utility
+    figlet
+    macchina
+    fastfetch
+
+    # Audio & USB utils
+    alsa-utils
+    usbutils
   ];
 }
