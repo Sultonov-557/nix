3f02962a3ac3c0e4f6e0b83803ce6c9e1b609fd7
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-11-19 11:35:25 +0500

    fixed ghostty keybinds

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -4,5 +4,14 @@
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
+    clearDefaultKeybinds = true;
+
+    settings = {
+      keybind = [
+        "copy=copy_to_clipboard"
+        "paste=paste_from_clipboard"
+        "ctrl+shift+v=paste_from_clipboard"
+      ];
+    };
   };
 }
