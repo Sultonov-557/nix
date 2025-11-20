5015fe483fd95d21961f9656b987bf947aaff75f
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-11-20 17:32:40 +0500

    git improvments

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -8,9 +8,30 @@
       user.email = "sultonovzerifboy@gmail.com";
       init.defaultBranch = "main";
       pull.rebase = false;
+      push.autoSetupRemote = true;
+      rerere.enabled = true;
       safe.directory = [
         "*"
       ];
+    };
+
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+      type = "cat-file -t";
+      dump = "cat-file -p";
+    };
+
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        line-numbers = true;
+        syntax-theme = "Dracula";
+      };
     };
   };
 
