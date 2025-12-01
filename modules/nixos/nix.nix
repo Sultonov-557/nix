507a61d5df0a76c47cb4e93f3f0949042267ba39
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-12-01 16:41:14 +0500

    some caches

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -41,10 +41,13 @@
     ];
     substituters = [
       "https://cache.nixos.org/"
+      "https://nixpkgs.cachix.org"
       "https://hyprland.cachix.org"
-      "https://cache.nix.m7.rs"
     ];
     trusted-public-keys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:ZFI3rT84a1xr0EJ4yqVRK6p+Kx2HuhVE3rVb9VQwC5k="
       "niri:7kPRy+bwoMP2GiLMR20qHUUiQ1Tg96YapZFsyvokm90="
     ];
